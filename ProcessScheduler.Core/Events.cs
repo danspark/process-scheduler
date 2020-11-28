627e41870859c0051492424337115be2ad63dfc5
Daniel Reis <daniel.amorim@stone.com.br>
2020-11-28 12:34:24 -0300

    Adding events

---
+++ ProcessScheduler.Core/Events.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProcessScheduler.Core
+{
+    public record ProcessEventArgs(Process Process, TimeSpan CurrentTime);
+
+    public record ProcessExecutionEventArgs(Process Process, TimeSpan CurrentTime, TimeSpan Duration) 
+        : ProcessEventArgs(Process, CurrentTime);
+
+    public delegate void ProcessCreated(ProcessEventArgs args);
+
+    public delegate void ProcessFinished(ProcessEventArgs args);
+    
+    public delegate void ProcessExecutionStarted(ProcessExecutionEventArgs args);
+                                                
+    public delegate void ProcessExecutionStopped(ProcessExecutionEventArgs args);
+}
