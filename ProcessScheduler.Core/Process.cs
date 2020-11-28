fa794e3389d9a0b90734db429ac8e26626704587
Daniel Reis <daniel.amorim@stone.com.br>
2020-11-28 12:34:32 -0300

    Implementing Process class

---
+++ ProcessScheduler.Core/Process.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProcessScheduler.Core
+{
+    public class Process// (int Id, string Name, string User, TimeSpan SubmissionTime, TimeSpan TotalExecutionTime)
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; } = "";
+
+        public string User { get; init; } = "";
+
+        public TimeSpan SubmissionTime { get; init; }
+
+        public TimeSpan CurrentExecutionTime { get; private set; }
+
+        public TimeSpan TotalExecutionTime { get; init; }
+
+        public bool IsCompleted() => CurrentExecutionTime == TotalExecutionTime;
+
+        public TimeSpan ExecuteFor(TimeSpan duration)
+        {
+            var executionTime = CurrentExecutionTime + duration;
+
+            if (executionTime > TotalExecutionTime)
+            {
+                duration = TotalExecutionTime - executionTime;
+                
+                executionTime = TotalExecutionTime;
+            }
+
+            CurrentExecutionTime = executionTime;
+
+            return duration;
+        }
+    }
+}
